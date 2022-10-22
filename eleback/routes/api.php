b466033f4f55aaea1decdbbeb341b0bdb7408111
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-10-21 21:09:01 -0400

    subir

--- eleback/routes/api.php
+++ eleback/routes/api.php
@@ -20,11 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('login',[\App\Http\Controllers\UserController::class,'login']);
 Route::resource('/denuncia',\App\Http\Controllers\DenunciaController::class);
 Route::resource('/consulta',\App\Http\Controllers\ConsultaController::class);
-Route::group(['middleware'=>'auth:sanctum'],function (){
+    Route::resource('/poste',\App\Http\Controllers\PosteController::class);
+    Route::group(['middleware'=>'auth:sanctum'],function (){
     Route::post('logout',[\App\Http\Controllers\UserController::class,'logout']);
     Route::post('me',[\App\Http\Controllers\UserController::class,'me']);
     Route::resource('/punto',\App\Http\Controllers\PuntoController::class);
-    Route::resource('/poste',\App\Http\Controllers\PosteController::class);
 
 });
 
